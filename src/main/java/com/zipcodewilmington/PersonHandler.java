e114d00a0d8e2cb2c6e01c02e5bf0be216dcacd8
Isaac <isaac@Zipcoders-MacBook-Pro-2.local>
2025-03-08 17:19:28 -0500

    alltests passed completed

--- src/main/java/com/zipcodewilmington/PersonHandler.java
+++ src/main/java/com/zipcodewilmington/PersonHandler.java
@@ -16,14 +16,19 @@ public class PersonHandler {
     public String whileLoop() {
         String result = "";
         // create a `counter`
+        int counter = 0;
         // while `counter` is less than length of array
+        while (counter < personArray.length){
             // begin loop
-
-                // use `counter` to identify the `current Person` in the array
-                // get `string Representation` of `currentPerson`
-                // append `stringRepresentation` to `result` variable
+           result += personArray[counter];
+            counter++;
+            // use `counter` to identify the `current Person` in the array
+            // get `string Representation` of `currentPerson`
+            // append `stringRepresentation` to `result` variable
 
             // end loop
+        }
+
         return result;
     }
 
@@ -34,7 +39,9 @@ public class PersonHandler {
         // identify initial value
         // identify terminal condition
         // identify increment
-
+        for (int i = 0; i < personArray.length; i++) {
+            result += personArray[i];
+        }
         // use the above clauses to declare for-loop signature
             // begin loop
                 // use `counter` to identify the `current Person` in the array
@@ -51,7 +58,9 @@ public class PersonHandler {
         String result = "";
         // identify array's type
         // identify array's variable-name
-
+        for (Person var: personArray){
+            result += var;
+        }
         // use the above discoveries to declare for-each-loop signature
             // begin loop
                 // get `string Representation` of `currentPerson`
